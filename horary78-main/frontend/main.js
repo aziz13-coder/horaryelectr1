f69da7479cbc92cc8f58a1f9b3a06bcfa56fe87e
aziz13-coder <123633037+aziz13-coder@users.noreply.github.com>
2025-06-08 12:14:52 +0300

    feat: improve electron dev detection

--- horary78-main/frontend/main.js
+++ horary78-main/frontend/main.js
@@ -2,9 +2,19 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const { spawn } = require('child_process');
 
+// Determine if we are running in development mode.
+const isDev = !app.isPackaged;
+
 function createWindow() {
   const win = new BrowserWindow({ width: 800, height: 600 });
-  win.loadFile(path.join(__dirname, 'dist/index.html'));
+
+  if (isDev) {
+    // In development we load the Vite dev server.
+    win.loadURL('http://localhost:3000');
+  } else {
+    // In production we load the built index.html.
+    win.loadFile(path.join(__dirname, 'dist/index.html'));
+  }
 }
 
 app.whenReady().then(() => {
